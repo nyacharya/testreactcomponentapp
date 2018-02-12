8723f4c1bbba2fd9c835c623b9103bf7bf753232
nayan <nayanacharya9@gmail.com>
2018-02-12 19:21:31 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Fancy from 'react-fancy-component';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       
+        <Fancy/>
+      </div>
+    );
+  }
+}
+
+export default App;
